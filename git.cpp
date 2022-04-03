43fc80b46dc9d1a29fc8f44cef0012e146f736a6
Abhi <abhishek0068.be21@chitkara.edu.in>
2022-04-03 12:38:42 +0530

    We could find sum of two numbers

--- git.cpp
+++ git.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"Hello World";
+	int a,b,c;
+	cout<<"Enter  a digit\n";
+	cin>>a;
+	cout<<"enter another digit\n";
+	cin>>b;
+	c=a+b;
+	cout<<"Sum of both digit is "<<c;
 }
